a93eb3d8f547c2c8d630e9930e0aff79a02851f2
jayu <coding@slightboy.net>
2016-06-24 19:24:07 +0800

    temp

--- snippet/proxy.js
+++ snippet/proxy.js
@@ -39,65 +39,102 @@ const ignoredProperties = {
 
 let schema = require('./modelLoader')
 
+const ArrayFunctions = new Set(['splice', 'push', 'pop', 'unshift', 'shift'])
 
-let p = EpicProxy(group);
-p.managers.push(11111);
-console.log(p.managers, p.managers.length);
+// https://github.com/anywhichway/proxy-observe/blob/master/index.js
+const ArrayHandler = path => ({
+		get (target, name, receiver) {
+			if (ArrayFunctions.has(name))
+				return function () {
+					return Reflect.apply(Array.prototype[name], target, arguments);
+				};
+
+
+			return target[name];
+		},
+		set (target, name, value, receiver) {
+			console.log('set', arguments);
+			return true;
+		},
+		deleteProperty (target, name) {
+			console.log('del', arguments);
+			return true;
+		}
+	}
+);
+
+
+let p = EpicProxy([1,2,3], ArrayHandler());
+
+p[0] = 111;
+
+console.log(p);
+
+
+const ObjectHandler = path => ({
+	get (target, name, receiver) {
+		// debug('get', name, typeof(target[name]));
+		if (typeof(target[name]) === 'object') return new Proxy(target[name], handler((path || []).concat(name)))
+
+		return target[name];
+	},
+	set (target, name, value, receiver) {
+						console.log(path);
+		let property = schema.cache.get(path.join('.'));
+		if (isIgnored(property, name)) return true;
 
 
 
+		if (!target.hasOwnProperty(name))
+			changes.push(['add', path, name, target[name], value]);
+		else
+			changes.push(['set', path, name, target[name], value]);
+		debug(changes[changes.length - 1]);
+		target[name] = value;
+		return true;
+	},
+	deleteProperty (target, name) {
+		changes.push(['del', path, name, target[name]]);
+		debug(changes[changes.length - 1]);
+
+		let property = schema.cache.get(path.join('.'));
+		if (property && property.type === 'array')
+			target.splice(name);
+		else
+			delete target[name];
+
+		return true;
+	}
+});
+
+const findProperty = (path, name) => {
+	let result = schema.cache.get(path.join('.'));
+	if (!result) return;
+
+};
+
+const isIgnored = (property, name) => {
+	if (!property) return;
+	return ignoredProperties[property.type] && ignoredProperties[property.type].has(name);
+};
 
 function EpicProxy (data, handler) {
-
-	let findProperty = (path, name) => {
-		let result = schema.cache.get(path.join('.'));
-		if (!result) return;
-
-	};
-
-	let isIgnored = (property, name) => {
-		if (!property) return;
-		return ignoredProperties[property.type] && ignoredProperties[property.type].has(name);
-	}
-
 	let changes = [];
 
-	let defaultHandler = path => ({
-			get (target, name, receiver) {
-				// debug('get', name, typeof(target[name]));
-				if (typeof(target[name]) === 'object') return new Proxy(target[name], handler((path || []).concat(name)))
 
-				return target[name];
-			},
-			set (target, name, value, receiver) {
-				let property = schema.cache.get(path.join('.'));
-				if (isIgnored(property, name)) return true;
 
-				if (!target.hasOwnProperty(name))
-					changes.push(['add', path, name, target[name], value]);
-				else
-					changes.push(['set', path, name, target[name], value]);
-				debug(changes[changes.length - 1]);
-				target[name] = value;
-				return true;
-			},
-			deleteProperty (target, name) {
-				changes.push(['del', path, name, target[name]]);
-				debug(changes[changes.length - 1]);
 
-				let property = schema.cache.get(path.join('.'));
-				if (property && property.type === 'array')
-					target.splice(name);
-				else
-					delete target[name];
-
-				return true;
-			}
-		}
-	);
-
-	handler = handler || defaultHandler;
-
-	return new Proxy(data, handler());
+	return new Proxy(data, handler);
 
 }
+
+/*
+object:
+{name: '', object: {}, type: '', oldValue: ''};
+["add", "update", "delete", "reconfigure", "setPrototype", "preventExtensions"]
+
+array:
+{name: '', object: {}, type: '', oldValue: '', index: 0, removed: '', addedCount: ''};
+["add", "update", "delete", "splice"]
+// [{type: 'splice', object: <arr>, index: 1, removed: ['B', 'c', 'd'], addedCount: 3}]
+*/
